a994061044cf4388c6697dbdf2813e24677cf495
Looserseeman <101535833+Looserseeman@users.noreply.github.com>
2022-09-06 15:58:09 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -260,7 +260,7 @@ def start(update, context):
         else:
             sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        text = f"Not Authorized user, deploy your own mirror bot"
+        text = f"<b>ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜsᴇʀ, ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ᴍɪʀʀᴏʀ ʙᴏᴛ ᴏʀ ғᴜᴄᴋᴏғғ...</b>"
         if PICS:
             sendPhoto(text, context.bot, update.message, random.choice(PICS), reply_markup)
         else:
